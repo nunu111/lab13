317850bfadf84759d3ebca22692f4ccdee6d0efd
nunu111 <nunu111za@hotmail.com>
2021-12-22 14:33:43 +0700

    lab13_4 done

--- lab13_4.cpp
+++ lab13_4.cpp
@@ -2,6 +2,9 @@
 
 using namespace std;
 
+template <typename home>
+void mySwap(home&,home&);
+
 int main(){
 	int x, y;
 	string a, b;
@@ -30,3 +33,9 @@ int main(){
 	return 0;
 }
 
+template <class home>
+void mySwap(home &x,home &y){
+	home z=x;
+	x=y;
+	y=z;
+}
